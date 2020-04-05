2bdbe9c55eb1c9079259e94824e9e6278f61db7d
danst3in <50937807+danst3in@users.noreply.github.com>
2020-04-05 14:05:59 -0700

    create class components. add weird babel required features for state to define without constructor and super

---
+++ adopt-me/src/Carousel.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+class Carousel extends Component {
+  state = {
+    photos: [],
+    active: 0,
+  };
+
+  // this function takes in a set of props, filters them, passes them on to the component as updated state
+  static getDerivedStateFromProps({ media }) {
+    let photos = ["http://placecorgi.com/600/600"];
+
+    if (media.length) {
+      photos = media.map(({ large }) => large);
+    }
+
+    // return the object we want merged into the state
+    return { photos };
+  }
+
+  render() {
+    const { photos, active } = this.state;
+    return (
+      <div className="carousel">
+        <img src={photos[active]} alt="animal" />
+        <div className="carousel-smaller">
+          {photos.map((photo, index) => (
+            // eslint-disable-next-line
+            <img
+              key={photo}
+              onClick={this.handleIndexClick}
+              data-index={index}
+              src={photo}
+              className={index === active ? "active" : ""}
+              alt="animal thumbnail"
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
