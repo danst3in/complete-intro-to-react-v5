5aedc1cda86b0bb4e8ad3ecd798fcf09c374d241
danst3in <50937807+danst3in@users.noreply.github.com>
2020-04-04 20:49:51 -0700

    Move components to seperate files and add parcel dev server

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,27 +1,21 @@
 import React from "react";
 import { render } from "react-dom";
+import Pet from "./Pet";
 
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
-  ]);
-};
 const App = () => {
   return React.createElement("div", { id: "something-important" }, [
     React.createElement("h1", {}, "Adopt Me!"),
     React.createElement(Pet, {
       name: "Luna",
       animal: "Dog",
-      breed: "Havanese"
+      breed: "Havanese",
     }),
     React.createElement(Pet, {
       name: "Pepper",
       animal: "Bird",
-      breed: "Cockatiel"
+      breed: "Cockatiel",
     }),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed" })
+    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed" }),
   ]);
 };
 render(React.createElement(App), document.getElementById("root"));
