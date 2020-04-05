57f11d087abb4c8149f96afb2234804283b93cd2
danst3in <50937807+danst3in@users.noreply.github.com>
2020-04-05 10:48:26 -0700

    switch to mock api. select pet from api. pet results

--- adopt-me/src/SearchParams.js
+++ adopt-me/src/SearchParams.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
+import Results from './Results'
 import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
@@ -7,6 +8,17 @@ const SearchParams = () => {
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
   const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, usePets] = useState([]);
+
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal,
+    });
+
+    setPets(animals || []);
+  }
 
   useEffect(() => {
     // this promise will render after the intiial page render
@@ -25,7 +37,12 @@ const SearchParams = () => {
   return (
     <div className="search-params">
       <h1>{location}</h1>
-      <form>
+      <form
+        onSumbit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
@@ -39,6 +56,7 @@ const SearchParams = () => {
         <BreedDropdown />
         <button>Submit</button>
       </form>
+      <Results pets={pets}
     </div>
   );
 };
