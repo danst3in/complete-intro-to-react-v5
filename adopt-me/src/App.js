a1ffa89a422eb375bf4c4038b778dc9f6667be9f
danst3in <50937807+danst3in@users.noreply.github.com>
2020-04-04 18:30:56 -0700

    created simple components using react.createElement

---
+++ adopt-me/src/App.js
@@ -0,0 +1,15 @@
+Const Pet = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Luna"),
+    React.createElement("h2", {}, "Dog"),
+    React.createElement("h2", {}, "Havanese"),
+  ])
+}
+const App = () => {
+  return React.createElement(
+    "div",
+    {id: "something-important"},
+    React.createElement("h1", {}, "Adopt Me!")
+  );
+};
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
