ef44257f91503ed18f25ce20f9b2b25a2b7cc76d
danst3in <50937807+danst3in@users.noreply.github.com>
2020-04-05 01:59:35 -0700

    added strict mode

--- adopt-me/src/SearchParams.js
+++ adopt-me/src/SearchParams.js
@@ -1,17 +1,31 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+
+  useEffect(() => {
+    // this promise will render after the intiial page render
+    // will continue to run after each time render is called
+    // clear breeds when switching animal types
+    setBreeds([]);
+    setBreed("");
+
+    // get breeds list from the API , get destructured name from each breed object
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStrings = breeds.map(({ name }) => name);
+      setBreeds(breedStrings);
+    }, console.error);
+  }, [animal, setBreed, setBreeds]); // array of dependencies that if changed will cause useEffect to fire after component render
 
   return (
     <div className="search-params">
       <h1>{location}</h1>
-      <form action="">
+      <form>
         <label htmlFor="location">
           Location
           <input
